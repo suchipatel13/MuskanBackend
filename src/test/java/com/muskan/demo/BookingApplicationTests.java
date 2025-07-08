427bd83e5196528abfc94898c09b90435aaf40eb
SUCHI <svp@udel.edu>
2025-07-08 18:54:44 -0400

    Initial commit

---
+++ src/test/java/com/muskan/demo/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muskan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
